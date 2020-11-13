80489cbb4bb5cfc6a7a009dbfad65bf8e4016cc1
Jeffrey Hirai <jhirai20@gmail.com>
2020-11-12 18:53:05 -0600

    lvl 3 complete

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -72,13 +72,10 @@ namespace SportsORM.Controllers
 
             ViewBag.SophiaTeams=_context.Teams.Include(t=>t.CurrentPlayers).Where(t=>t.CurrentPlayers.Any(p=>p.FirstName=="Sophia")).ToList();
 
-            // var SophiaLeague=_context.Teams.Include(t=>t.CurrentPlayers).Where(p=>p.CurrentPlayers.Any(p=>p.FirstName=="Sophia")).ToList();
-            // ViewBag.SophiaLList=new List<String>();
-            // foreach (var item in SophiaLeague)
-            // {
-            //     ViewBag.SophiaLList.Add(item.CurrLeague.Name.ToString());
-            // };
-                //Don't know how to deal with null entries I tried if statments and toString convertions but it doesnt work.
+            ViewBag.SophiaLList=_context.Teams.Include(t => t.CurrentPlayers)
+                                      .Where(t => t.CurrentPlayers
+                                      .Any(p => p.FirstName == "Sophia"))
+                                      .ToList();
 
             ViewBag.AllFlores=_context.Players.Include(p =>p.CurrentTeam).Where(p =>p.CurrentTeam.TeamName != "Roughriders" && p.LastName =="Flores").ToList();
             return View();
@@ -87,6 +84,15 @@ namespace SportsORM.Controllers
         [HttpGet("level_3")]
         public IActionResult Level3()
         {
+            ViewBag.Evans=_context.Players.Include(p=>p.AllTeams).ThenInclude(t=>t.TeamOfPlayer).FirstOrDefault(p => p.FirstName == "Samuel" && p.LastName == "Evans");
+            ViewBag.two=_context.Teams.Include(p=>p.AllPlayers).ThenInclude(p=>p.PlayerOnTeam).FirstOrDefault(t=>t.Location=="Manitoba" && t.TeamName=="Tiger-Cats");
+            ViewBag.three=_context.PlayerTeams.Include(p=>p.PlayerOnTeam).ThenInclude(t=>t.CurrentTeam).Include(t=>t.TeamOfPlayer).Where(pt => pt.TeamOfPlayer.Location == "Wichita" && pt.TeamOfPlayer.TeamName == "Vikings") .Where(pt => pt.PlayerOnTeam.CurrentTeam.Location != "Wichita" && pt.PlayerOnTeam.CurrentTeam.TeamName != "Vikings");
+            ViewBag.four=_context.PlayerTeams.Include(p=>p.PlayerOnTeam).ThenInclude(p=>p.CurrentTeam).Include(p=>p.TeamOfPlayer).Where(p=>p.PlayerOnTeam.FirstName== "Jacob" && p.PlayerOnTeam.LastName=="Gray").Where(p=>p.TeamOfPlayer.Location != "Oregon" &&p.TeamOfPlayer.TeamName!="Colts");
+            ViewBag.five=_context.PlayerTeams.Include(t=>t.TeamOfPlayer).ThenInclude(p=>p.CurrLeague).Include(p=>p.PlayerOnTeam).Where(p=>p.PlayerOnTeam.FirstName== "Levi").Where(p=>p.TeamOfPlayer.CurrLeague.Name=="Atlantic Federation of Amateur Baseball Players").ToList();
+            ViewBag.six=_context.Teams.Include(t=>t.CurrentPlayers).Include(p=>p.AllPlayers).Where(p => p.CurrentPlayers.Count+ p.AllPlayers.Count >=12).ToList();
+            ViewBag.seven=_context.Players.Include(t=>t.AllTeams).OrderByDescending(p => p.AllTeams.Count).ToList();
+
+
             return View();
         }
 
